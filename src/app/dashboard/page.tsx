bb5d8e6dd7af690ad85874681adc234ca8d4c583
Ma1y0 <matyas.barr@gmail.com>
2024-01-08 13:09:19 +0100

    Dashboard navigation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
-export default function Home() {
+export default async function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
-      <Link href="/dashboard">-o-o-o-</Link>
     </main>
   );
 }
